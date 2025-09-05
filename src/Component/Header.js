6b561b44c45ee36e5a8fb4d0850369bcb57e13da
ashish0jha <ashish2893jnv@gmail.com>
2025-09-05 12:30:31 +0530

    Storing Users Data in Redux Store

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,23 +1,60 @@
-const Header = ({clickHandler}) => {
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {onAuthStateChanged, signOut} from "firebase/auth";
+import {auth} from "../utils/firebase"
+import { addUser, removeUser } from "../utils/userSlice";
+import { useEffect } from "react";
+
+const Header = () => {
+  const user=useSelector((store)=>store.user);
+  const navigate=useNavigate();
+
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+      const unsubscribe=onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const {uid,email,displayName,photoURL} = user;
+        dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
+        navigate("/Browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    //unsubscribing the onAuthStateChanged when component is unmounted.
+    return ()=>{unsubscribe()};
+  },[])
 
   return (
-    <div className="absolute w-full px-[110px] py-2 bg-gradient-to-b from-black flex text-white justify-between">
+    <div className="absolute w-full z-50 px-[110px] py-2 bg-gradient-to-b from-black flex text-white justify-between">
         <div>
             <img 
             className="w-44"
             src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
         </div>
-        <div>
-            <select className="m-3 px-3 py-1 rounded-md border border-white hover:border-2 bg-transparent font-semibold">
-                    <option value="English" className="text-black"> English</option>
-                    <option value="Hindi" className="text-black">Hindi</option>
-            </select>
+        {user && <div className="flex justify-center items-center">
+            <div className="flex flex-col items-center">
+              <img 
+              src={user.photoURL}
+              className="w-10 h-9 rounded-sm"
+              alt="User Profile"
+              />
+              <p>{user.displayName}</p>
+            </div>
             
             <button 
             className="m-2 px-3 rounded-md font-semibold py-1 bg-red-600"
-            onClick={clickHandler}
-            >Sign In</button> 
-        </div>
+            onClick={()=>{
+              signOut(auth).then(() => {
+                  // Sign-out successful.
+
+                }).catch((error) => {
+                  // An error happened.
+                });
+            }}
+            >Sign Out</button> 
+        </div>}
         
     </div>
   )
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,12 +1,16 @@
 import Header from "./Header";
 import { useState , useRef } from "react";
 import {checkValidation} from "../utils/validate";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword  } from "firebase/auth";
-import { auth } from "../utils/firebase"
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword ,updateProfile } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Home = () => {
   const [loginbtn,setloginbtn]=useState(true);
   const [signup,setsignup]=useState(false);
+
+  const dispatch=useDispatch();
 
   const [Validationmsg,setValidationmsg]=useState();
 
@@ -32,24 +36,39 @@ const Home = () => {
         .then((userCredential) => {
           // Signed up 
           const user = userCredential.user;
-          console.log(user);
-          // ...
+
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
+
+          }).then(() => {
+            const {uid,email,displayName,photoURL} = auth.currentUser;
+            dispatch(
+              addUser({
+                uid:uid,
+                email:email,
+                displayName:displayName,
+                photoURL:photoURL
+              }))
+          }).catch((error) => {
+            setValidationmsg(error.message);
+          });
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          // ..
           setValidationmsg(errorCode+" "+errorMessage);
       });
 
     }
+    //signing in
     else{
       signInWithEmailAndPassword(auth,email.current.value,password.current.value)
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
-        console.log("dxfcgvhj",user);
-        // ...
+        console.log(user);
+        
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -62,17 +81,19 @@ const Home = () => {
   return (
 
     <div>
-        
-        <Header clickHandler={()=>setloginbtn(false)}/>
+        <Header/>
         <div>
             <img
-            src="https://assets.nflxext.com/ffe/siteui/vlv3/cb72daa5-bd8d-408b-b949-1eaef000c377/web/IN-en-20250825-TRIFECTA-perspective_a3209894-0b01-4ddb-b57e-f32165e20a3f_large.jpg" alt="bg"/>
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/cb72daa5-bd8d-408b-b949-1eaef000c377/web/IN-en-20250825-TRIFECTA-perspective_a3209894-0b01-4ddb-b57e-f32165e20a3f_large.jpg" 
+            alt="bg"
+            className="brightness-[26%]"
+            />
         </div>
         <div>
             {
               loginbtn===true 
-              ? <div  className="absolute top-1/4 left-1/3 w-5/12 flex flex-col items-center p-10 bg-black bg-opacity-30 rounded-lg">
-                  <h1 className="font-bold text-white text-5xl text-center">Unlimited movies ,TV shows and more</h1>
+              ? <div  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
+                  <h1 className="font-bold text-white text-6xl text-center">Unlimited movies ,TV shows and more</h1>
                   <h3 className="font-semibold text-white text-center my-6 text-2xl">Starts at ₹149. Cancel at any time.</h3>
                   <p className="text-white text-lg">Ready to watch? Enter your email to create or restart your membership.</p>
                   <button className="m-2 p-8 py-3 rounded-md font-semibold bg-red-600 hover:bg-red-700 text-white text-2xl"
